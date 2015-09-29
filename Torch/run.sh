9d72dbbbe32714a2483d98a6ce6eccc6508ff3f8
Adam Summerville <adamsumm@gmail.com>
2015-09-29 12:32:56 -0700

    Easy running now

---
+++ Torch/run.sh
@@ -0,0 +1,1 @@
+th sample.lua smbLineNo_512_3_0.8_10_0.002_0.97_800/lm_lstm_epoch5.00_0.0189.t7 -primetext ":(-------------XXX)XXX*------------(------------*XXX" -seed $1 -length 4400 > temp; python parseBackwordsForwards.py 
